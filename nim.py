5811aaea17eefe417a4c58f9a18e552c5b780a5b
mb6ff <mariebiernacki.art@gmail.com>
2025-09-12 13:35:31 -0500

    initial commit -- pseudocode

---
+++ nim.py
@@ -0,0 +1,51 @@
+# Prologue Comments
+# Title: EECS 348 Assignment 3
+# Description: 2-player game of nim
+
+# Inputs: Player1 row number and stars to remove, Player2 row number and stars to remove
+# Outputs: Displays the current game board to the players in the console or errors if appropriate
+
+# Collaborators: None
+# Sources: ChatGPT, FIXME probably more
+
+# Author: Marie Biernacki
+# Creation Date: September 12th, 2025
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print("Welcome to nim!")
+
+    #display the starting board
+        # check if game is finished
+        # if yes, display the current player as the winner (they made the board empty) and end loop
+        # otherwise continue
+
+    # prompt player 1
+        #display Player 1
+        # ask player 1 for row num
+        # ask player 1 for stars to remove
+
+        # determine if the move is valid
+            # if yes
+                # update the board and display
+                    # check if game is finished
+                    # if yes, display the current player as the winner (they made the board empty) and end loop
+                    # otherwise continue
+            # if no
+                # error
+                # re-display the current board and re-prompt current player
+
+    # prompt player 2
+        # display Player 2
+        # ask player 2 for row num
+        # ask player 2 for stars to remove
+
+        # determine if the move is valid
+            # if yes
+                # update the board and display
+                    # check if game is finished
+                    # if yes, display the current player as the winner (they made the board empty) and end loop
+                    # otherwise continue
+            # if no
+                # error
+                # re-display the current board and re-prompt current player
