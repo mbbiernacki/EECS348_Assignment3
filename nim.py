1a6461c34c48c45fabdf209375900d3c3ac63313
mb6ff <mariebiernacki.art@gmail.com>
2025-09-12 14:33:30 -0500

    added displayCurrentBoard

--- nim.py
+++ nim.py
@@ -11,9 +11,52 @@
 # Author: Marie Biernacki
 # Creation Date: September 12th, 2025
 
-# Press the green button in the gutter to run the script.
+def updateBoard(currentBoard, rowNum, starNum):
+    #update the current board
+
+def playerPrompt(playerNum):
+    # collect player choices here?
+
+# function to determine if the current move is valid
+def isValidMove(currentBoard, rowNum, starNum):
+    #FIXME check that the rowNum entered is valid
+    # check the stars can be removed from that row
+
+# function to display the current nim board
+# SOURCE: Myself
+def displayCurrentBoard(currentBoard):
+
+    # iterate through the 2D array of the currentBoard using a for loop
+    # first, iterate through each row
+    for row in range(len(currentBoard)):
+        # print the row number
+        print(f"{row + 1}:", end=" ")
+
+        # iterate through each star of that row
+        for col in range(len(currentBoard[row])):
+            # print the star at [row][col]
+            # use end=" " to ensure they are on the same line
+            print(currentBoard[row][col], end=" ")
+
+        # after iterating through the row, create a newline for the next row
+        print()
+
+# function to initialize and play a game of nim until the user decides to quit
+# SOURCE: Myself
+def playNim():
+
+    # use a 2D array to define the initial game board
+    currentBoard = [
+        ['*','*','*','*','*'],
+        ['*','*','*','*'],
+        ['*','*','*'],
+        ['*','*'],
+        ['*']]
+
+    displayCurrentBoard(currentBoard)
+
 if __name__ == '__main__':
-    print("Welcome to nim!")
+    playNim()
 
     #display the starting board
         # check if game is finished
